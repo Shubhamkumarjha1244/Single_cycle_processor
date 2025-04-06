4f5d5a4fe334faef19d60c576c5ad6927d97b841
Shubhamkumarjha1244 <jha.kumar.shubham1244@gmail.com>
2025-04-06 17:18:59 +0530

    commit 2--memory,register,extend block created

--- single_cycle_processor.srcs/sim_1/new/test_tb.v
+++ single_cycle_processor.srcs/sim_1/new/test_tb.v
@@ -68,31 +68,181 @@ module test_tb();
 //        end    
         
         
-        //adder test
-        reg[31:0] inn1,inn2;
+//        //adder test
+//        reg[31:0] inn1,inn2;
+//        wire[31:0] out;
+        
+//        adder_32bit dut(inn1,inn2,out);
+        
+//        initial
+//            begin
+//                inn1=32'd5;
+//                inn2=32'd7;
+//                #5
+//                inn1=32'd10;
+//                inn2=32'd22;
+//                #5
+//                inn1=32'd244;
+//                inn2=32'd366;
+//                #5
+//                $finish;
+//            end
+
+
+//        //instruction memory test
+//        reg [31:0] addr;
+//        wire [31:0] data;
+        
+//        Instruction_memory dut(addr,data);
+        
+//        initial
+//            begin
+//                addr=32'd0;
+//                #10
+//                addr=32'd1;
+//                #10
+//                addr=32'd2;
+//                #10
+//                addr=32'd3;
+//                #10
+//                addr=32'd4;
+//                #10
+//                addr=32'd5;
+//                #10
+//                $finish;
+//            end
+            
+            
+//        //data memory test
+        
+//        reg[31:0] addr;
+//        reg[31:0] write_data;
+//        reg write_enable;
+//        reg clk=0;
+
+        
+//        wire [31:0] read_data;
+        
+//        data_memory dut(clk,write_enable,addr,write_data,read_data);
+        
+//        always
+//            #5 clk=~clk;
+        
+//        initial 
+//                begin
+//                    #2
+//                    write_enable=1'b0;
+//                    addr=32'd1;
+//                    #10
+//                    write_enable=1'b1;
+//                    addr=32'd0;
+//                    write_data=32'd11;
+//                    #10
+//                    write_enable=1'b1;
+//                    addr=32'd1;
+//                    write_data=32'd21;
+//                    #10
+//                    write_enable=1'b1;
+//                    addr=32'd2;
+//                    write_data=32'd31;
+//                    #10
+//                    write_enable=1'b0;
+//                    addr=32'b0;
+//                    #10
+//                    write_enable=1'b0;
+//                    addr=32'b1;
+//                    #10
+//                    write_enable=1'b0;
+//                    addr=32'd10;                   
+//                    #10
+//                    write_enable=1'b0;
+//                    addr=32'd2;                   
+//                    #10
+//                    $finish;
+//               end
+
+
+//        //registor file test
+//        reg clk=0;
+//        reg write_enable;
+//        reg[4:0] addr1,addr2,addr3;
+//        reg[31:0] write_data;
+//        wire[31:0] read_data1,read_data2;
+         
+//        register_file dut(clk,write_enable,addr1,addr2,addr3,write_data,read_data1,read_data2);
+        
+//        always
+//            #5 clk=~clk;
+            
+//         initial
+//            begin
+//                write_enable=1'b1;
+//                addr1=5'd0;
+//                addr2=5'd1;
+//                addr3=5'd2;
+//                write_data=32'd5;
+//                #10
+//                write_enable=1'b1;
+//                addr1=5'd2;
+//                addr2=5'd1;
+//                addr3=5'd0;
+//                write_data=32'd10;
+//                #10
+//                write_enable=1'b1;
+//                addr1=5'd2;
+//                addr2=5'd0;
+//                addr3=5'd1;
+//                write_data=32'd20;
+//                #10
+//                write_enable=1'b0;
+//                addr1=5'd2;
+//                addr2=5'd0;
+//                addr3=5'd1;
+//                write_data=32'd20;
+//                #10
+//                write_enable=1'b0;
+//                addr1=5'd2;
+//                addr2=5'd1;
+//                addr3=5'd2;
+//                write_data=32'd20;
+//                #10
+//                write_enable=1'b1;
+//                addr1=5'd2;
+//                addr2=5'd1;
+//                addr3=5'd1;
+//                write_data=32'd121;
+//                #10
+//                write_enable=1'b0;
+//                addr1=5'd1;
+//                addr2=5'd0;
+//                addr3=5'd2;
+//                write_data=32'd121; 
+//                #10
+//                $finish;
+//            end                       
+        
+        
+        
+        //extend block test
+        reg imm_src;
+        reg[11:0] inn;
         wire[31:0] out;
         
-        adder_32bit dut(inn1,inn2,out);
+        extend_12_to_32 dut(imm_src,inn,out);
         
-        initial
+        initial 
             begin
-                inn1=32'd5;
-                inn2=32'd7;
-                #5
-                inn1=32'd10;
-                inn2=32'd22;
-                #5
-                inn1=32'd244;
-                inn2=32'd366;
-                #5
-                $finish;
-            end
-                
+                imm_src=1;
+                inn=32'hFFA;
+                #10
+                inn=32'h0FD;
+                #10
+                imm_src=0;
+                inn=32'hFFA;
+                #10 
+                $finish; 
+            end             
         
-      
-
-    
-
-    
-    
+        
+                    
 endmodule
--- single_cycle_processor.srcs/sources_1/new/single_cycle_processor.v
+++ single_cycle_processor.srcs/sources_1/new/single_cycle_processor.v
@@ -35,7 +35,7 @@ module d_ff_32bit(clk,inn,out); //dff for clock syncronization of single cycle p
     output reg[31:0] out;
     
     always @(posedge clk)
-        out=inn;
+        out<=inn;
 
 endmodule   //tested
 
@@ -46,6 +46,78 @@ module adder_32bit(inn1,inn2,out); //adding 2 32bit number
     assign out=inn1+inn2;
     
 endmodule
+
+module Instruction_memory(addr,out_data); //instruction memory as rom with 32bit address line for 32bit word
+    input[31:0] addr;
+    output[31:0] out_data;
+    
+    reg[31:0] inst_mem[31:0];
+      
+    assign out_data=inst_mem[addr];
+
+endmodule //tested
+
+module data_memory(clk,write_enable,address,write_data,read_data); //data memory as a single port ram with 32 bit address line for 32bit data word
+    input clk,write_enable;
+    input[31:0] address;
+    input[31:0] write_data;
+    output reg [31:0] read_data;
+    
+    reg[31:0] data_mem[31:0];
+    
+    always @(posedge clk)
+        begin
+            if(write_enable)
+                data_mem[address]<=write_data;
+            else
+                read_data<=data_mem[address];
+        end
+                
+endmodule //tested
+
+module register_file(clk,write_enable,addr1,addr2,addr3,write_data,read_data1,read_data2); //3 addr register file
+    input clk;
+    input write_enable;
+    input[4:0] addr1,addr2,addr3;
+    input[31:0] write_data;
+    output reg [31:0] read_data1;
+    output reg  [31:0] read_data2;
+    
+    reg[31:0] register_file[4:0];
+    
+    always @(posedge clk)
+        begin
+        read_data1<=register_file[addr1];
+        read_data2<=register_file[addr2];
+        if(write_enable)
+                register_file[addr3]<=write_data;
+        end
+endmodule //tested
+
+
+module extend_12_to_32(imm_src,inn,out); //use to extend 12bit to 32bit data
+    input[11:0] inn;
+    input imm_src;
+    output reg[31:0] out;
+    
+    always @(*)
+        if(imm_src)
+            begin
+            out[11:0]=inn;
+            out[31:12]={5'd20{inn[11]}};
+            end
+endmodule //tested
+
+
+    
+    
+    
+
+
+            
+        
+    
+    
 
 
 
